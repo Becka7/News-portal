0160184453bdf25ec17a181c2835a9ed51f3e41f
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-22 15:07:06 +0300

    generate equals and hashcode

--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Department {
+    private int id;
+    private String name;
+    private String description;
+    private int employees;
+    private static ArrayList<Department> mInstance = new ArrayList<>();
+
+    private Department(String name,String description,int employees){
+        this.name = name;
+        this.description = description;
+        this.employees = employees;
+        mInstance.add(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getEmployees() {
+        return employees;
+    }
+
+    public static ArrayList<Department> getmInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id && employees == that.employees && Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, employees);
+    }
 }
--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Users {
+    private int id;
+    private String name;
+    private int department_id;
+    private String role;
+    private static ArrayList<Users> mInstance = new ArrayList<>();
+
+    public Users(String name,String role,int department_id){
+        this.name=name;
+        this.role=role;
+        this.department_id=department_id;
+        mInstance.add(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public static ArrayList<Users> getmInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return id == users.id && department_id == users.department_id && Objects.equals(name, users.name) && Objects.equals(role, users.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, department_id, role);
+    }
 }
